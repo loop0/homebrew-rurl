47f15b4039899f95e66b6d63a2fcbd14de1be943
Bruno Ribeiro da Silva <bruno.devpod@gmail.com>
2021-11-20 16:31:34 -0500

    Update to v0.1.3

--- Formula/rurl.rb
+++ Formula/rurl.rb
@@ -1,9 +1,9 @@
 class Rurl < Formula
   desc "A curl-like tool but configured by json files`"
   homepage "https://github.com/loop0/rurl"
-  url "https://github.com/loop0/rurl/releases/download/v0.1.2/rurl_v0.1.2_x86_64-apple-darwin.tar.gz"
-  version "0.1.2"
-  sha256 "71a4318ddb1b59578939fa79a6caf8edb85e0daf54ada3f7db40e381ec7e695a"
+  url "https://github.com/loop0/rurl/releases/download/v0.1.3/rurl_v0.1.3_x86_64-apple-darwin.tar.gz"
+  version "0.1.3"
+  sha256 "3461c14f7cfb9a531495038f9a01e168785d9c3ef44227a4e006d5acdb27af0a"
   license ""
 
   def install
